03bfffd10bb43a4f97757093bcc015e89705c0b5
Cleber1812 <cleber1812roberto@gmail.com>
2024-01-27 15:58:31 -0300

    Rota deletar empresa apenas por quem a criou

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes/routes');
 const app = express();
 
@@ -7,6 +8,14 @@ require('dotenv').config({path: './config/.env'});
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(routes);
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", "X-PINGOTHER, Content-Type, Authorization");
+  app.use(cors());
+  next();
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {

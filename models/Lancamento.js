7f5d03b2d01fb22f4aa418d3c17f9a75d6bf0af0
Cleber1812 <cleber1812roberto@gmail.com>
2024-02-06 19:30:50 -0300

    Livro Razao

--- models/Lancamento.js
+++ models/Lancamento.js
@@ -1,16 +1,29 @@
-// const sequelize = require("sequelize");
-const { Sequelize, DataTypes } = require("sequelize");
+//const sequelize = require("sequelize");
+//const { Sequelize, DataTypes } = require("sequelize");
+const { DataTypes } = require("sequelize");
 
 // module.exports = (sequelize, Sequelize) =>{
 module.exports = (sequelize) =>{
     const Lancamento = sequelize.define("Lancamento", {
-        fk_id_empresa: Sequelize.INTEGER, 
-        data: Sequelize.DATE, 
-        descricao: Sequelize.STRING,
-        fk_id_conta_debito: Sequelize.INTEGER,
-        fk_id_conta_credito: Sequelize.INTEGER,
-        valor: Sequelize.FLOAT,
-        fk_id_usuario: Sequelize.INTEGER        
+        // fk_id_empresa: Sequelize.INTEGER, 
+        // data: Sequelize.DATE, 
+        // descricao: Sequelize.STRING,
+        // fk_id_conta_debito: Sequelize.INTEGER,
+        // fk_id_conta_credito: Sequelize.INTEGER,
+        // valor: Sequelize.FLOAT,
+        // fk_id_usuario: Sequelize.INTEGER, 
+        // // Virtual para que não seja persistido no banco
+        // valorDebitado: {type: DataTypes.VIRTUAL,},
+        // valorCreditado: {type: DataTypes.VIRTUAL,}, 
+        fk_id_empresa: DataTypes.INTEGER, 
+        data: DataTypes.DATE, 
+        descricao: DataTypes.STRING,
+        fk_id_conta_debito: DataTypes.INTEGER,
+        fk_id_conta_credito: DataTypes.INTEGER,
+        valor: DataTypes.FLOAT,
+        fk_id_usuario: DataTypes.INTEGER, 
+        valorDebitado: { type: DataTypes.VIRTUAL },
+        valorCreditado: { type: DataTypes.VIRTUAL },      
     })
 
     // Lancamento.associate = (models) => {
@@ -19,10 +32,18 @@ module.exports = (sequelize) =>{
 
     Lancamento.associate = (models) => {
         // Associação com a tabela Conta para conta de débito
-        Lancamento.belongsTo(models.Contas, { foreignKey: 'fk_id_conta_debito', as: 'contaDebito' });
+        Lancamento.belongsTo(models.Contas, { 
+            foreignKey: 'fk_id_conta_debito', 
+            as: 'contaDebito',
+            subQuery: false, // Evita incluir todos os campos da tabela Conta 
+        });
 
         // Associação com a tabela Conta para conta de crédito
-        Lancamento.belongsTo(models.Contas, { foreignKey: 'fk_id_conta_credito', as: 'contaCredito' });
+        Lancamento.belongsTo(models.Contas, { 
+            foreignKey: 'fk_id_conta_credito', 
+            as: 'contaCredito',
+            subQuery: false, // Evita incluir todos os campos da tabela Conta
+         });
 
         // Associação com a tabela Usuario
         Lancamento.belongsTo(models.Usuario, { foreignKey: 'fk_id_usuario', as: 'usuario' });

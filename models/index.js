de04ca04d3a86f770afabf26c0e3c0d21958c714
Cleber1812 <cleber1812roberto@gmail.com>
2024-03-16 01:06:55 -0300

    vercel_14

--- models/index.js
+++ models/index.js
@@ -6,31 +6,31 @@ const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const dotenv = require('dotenv'); // Importe o pacote dotenv
-dotenv.config({ path: path.resolve(__dirname, '../config/.env') });
+// dotenv.config({ path: path.resolve(__dirname, '../config/.env') });
 const env = process.env.NODE_ENV || 'development';
 
-// const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-// let sequelize;
-// if (config.use_env_variable) {
-//   sequelize = new Sequelize(process.env[config.use_env_variable], {
-//     dialect: 'mysql',
-//     dialectModule: require('mysql2')
-//   }, config );
-// } else {
-//   sequelize = new Sequelize(config.database, config.username, config.password,  
-//     {
-//       dialect: 'mysql',
-//       dialectModule: require('mysql2')
-//     }, config);
-// }
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], {
+    dialect: 'mysql',
+    dialectModule: require('mysql2')
+  }, config );
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password,  
+    {
+      dialect: 'mysql',
+      dialectModule: require('mysql2')
+    }, config);
+}
 
-const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: 'mysql',
-  dialectModule: require('mysql2')
-});
+// const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+//   host: process.env.DB_HOST,
+//   dialect: 'mysql',
+//   dialectModule: require('mysql2')
+// });
 
 fs
   .readdirSync(__dirname)
